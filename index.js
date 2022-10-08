03513781ab81ed63dd0d4b387c5cd8d56d9da10e
kevinlinpt <kevinlinpt+1@hotmail.com>
2022-10-08 15:41:09 -0400

    project init folder and folder structure set backend

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("hello world")
+const express = require('express')
+const app = express()
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+//pull
+const PORT = process.env.PORT || 8080;
+
+//CORS middleware
+app.use(cors());
+
+//Middleware to give access to req.body
+app.use(express.json());
+
+//routing for endpoints
+app.get()
+app.post()
+app.put()
+app.delete()
+
+app.listen(8080, () => {
+  console.log("Server is up and running on port 8080! 🚀")
+})
