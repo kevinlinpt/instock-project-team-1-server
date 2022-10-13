e68c1d8b6c6cda1ab9be568df21a3fb9b4242825
Caitlin Klosterman <97420891+croseklo@users.noreply.github.com>
2022-10-12 18:32:28 -0700

    get all warehouses route

--- routes/getallwarehouses/getAllWarehouses.js
+++ routes/getallwarehouses/getAllWarehouses.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const warehouses = require("./data/warehouses.json");
+const warehouses = require("../../data/warehouses.json");
 
 router.get("/", function (req, res) {
   res.send(warehouses);
