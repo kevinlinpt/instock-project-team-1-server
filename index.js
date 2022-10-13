4363ce4339b163739159387cffdab0ee1bb31ccf
Caitlin Klosterman <97420891+croseklo@users.noreply.github.com>
2022-10-12 18:23:53 -0700

    warehouses route

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-console.log("hello world")
-const express = require('express')
-const app = express()
+console.log("hello world");
+const express = require("express");
+const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
-
+const getAllWarehouses = require("./routes/getallwarehouses/getAllWarehouses");
 //pull
 const PORT = process.env.PORT || 8080;
 
@@ -15,11 +15,13 @@ app.use(cors());
 app.use(express.json());
 
 //routing for endpoints
-app.get()
-app.post()
-app.put()
-app.delete()
+// app.get();
+// app.post();
+// app.put();
+// app.delete();
+
+app.use("/warehouses", getAllWarehouses);
 
 app.listen(8080, () => {
-  console.log("Server is up and running on port 8080! 🚀")
-})
+  console.log("Server is up and running on port 8080! 🚀");
+});
---
+++ routes/getallwarehouses/getAllWarehouses.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const warehouses = require("./data/warehouses.json");
+
+router.get("/", function (req, res) {
+  res.send(warehouses);
+});
+module.exports = router;
