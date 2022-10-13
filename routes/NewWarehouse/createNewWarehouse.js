d6a375ca006a7099533850c98d79d766d943d3ed
kaushalluffa <webdevelopment852@gmail.com>
2022-10-12 23:40:39 -0400

    A22AB1-12 final touches and ticket is complete

--- routes/NewWarehouse/createNewWarehouse.js
+++ routes/NewWarehouse/createNewWarehouse.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-const validator = require("node-email-validation")
+const validator = require("node-email-validation");
 
 let savedWarehouses;
 fs.readFile("./data/warehouses.json", "utf-8", (err, data) => {
@@ -16,7 +16,7 @@ fs.readFile("./data/warehouses.json", "utf-8", (err, data) => {
 });
 
 router.post("/", function (req, res) {
-const validPhone = req.body.contact.phone.split("").length === 10
+  const validPhone = req.body.contact.phone.split("").length === 10;
   if (
     req.body.name &&
     req.body.address &&
@@ -24,7 +24,7 @@ const validPhone = req.body.contact.phone.split("").length === 10
     req.body.country &&
     req.body.contact.name &&
     req.body.contact.position &&
-   validPhone&&
+    validPhone &&
     validator.is_email_valid(req.body.contact.email)
   ) {
     savedWarehouses.push(req.body);
@@ -39,14 +39,12 @@ const validPhone = req.body.contact.phone.split("").length === 10
       }
     );
     return res.status(201).send(savedWarehouses);
-  } else if(!validator.is_email_valid(req.body.contact.email)) {
-    return res
-      .status(400)
-      .send(`Please provide the valid email addresss`);
-  } else if(!validPhone) {
+  } else if (!validator.is_email_valid(req.body.contact.email)) {
+    return res.status(400).send(`Please provide the valid email addresss`);
+  } else if (!validPhone) {
     return res.status(400).send("please provide a valid phone number");
-  }else{
-    return res.status(400).send("Please provide the appropriate details")
+  } else {
+    return res.status(400).send("Please provide the appropriate details");
   }
 });
 
