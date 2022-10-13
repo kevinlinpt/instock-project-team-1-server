360d1a99e4b0e990679e03553314b4361e0c8ef9
kaushalluffa <webdevelopment852@gmail.com>
2022-10-12 22:18:28 -0400

    A22AB1-12 created if check to make sure the request body has all the data otherwise send and error message

--- routes/NewWarehouse/createNewWarehouse.js
+++ routes/NewWarehouse/createNewWarehouse.js
@@ -13,21 +13,36 @@ fs.readFile("./data/warehouses.json", "utf-8", (err, data) => {
     console.log(error, "try catch");
   }
 });
-const a = {
-  name: "aa",
-};
+
 router.post("/", function (req, res) {
-  savedWarehouses.push(req.body);
-  fs.writeFile(
-    "./data/warehouses.json",
-    JSON.stringify(savedWarehouses, null, 2),
-    (err) => {
-      if (err) {
-        console.log(err);
+
+  if (
+    req.body.name &&
+    req.body.address &&
+    req.body.city &&
+    req.body.country &&
+    req.body.contact.name &&
+    req.body.contact.position &&
+    req.body.contact.phone &&
+    req.body.contact.email
+  ) {
+    savedWarehouses.push(req.body);
+
+    fs.writeFile(
+      "./data/warehouses.json",
+      JSON.stringify(savedWarehouses, null, 2),
+      (err) => {
+        if (err) {
+          console.log(err);
+        }
       }
-    }
-  );
-  return res.status(201).send(savedWarehouses);
+    );
+    return res.status(201).send(savedWarehouses);
+  } else {
+    return res
+      .status(400)
+      .send(`Please provide the appropriate details`);
+  }
 });
 
 module.exports = router;
