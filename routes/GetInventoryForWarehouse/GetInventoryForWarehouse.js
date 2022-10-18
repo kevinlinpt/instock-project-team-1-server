13ac47e3d2f1fdfc189656934017eff016249aa7
kevinlinpt <kevinlinpt+1@hotmail.com>
2022-10-18 16:05:10 -0400

    Created API to GET inventories for a given warehouse

---
+++ routes/GetInventoryForWarehouse/GetInventoryForWarehouse.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const inventoryItems = require("../../data/inventories.json");
+
+router.get("/", function (req, res) {
+  console.log(req.body);
+  if (req.body.warehouseID) {
+    const inventory = inventoryItems.filter(
+      (item) => item.warehouseID === req.body.warehouseID
+    );
+    res.send(inventory);
+  } else {
+    return res
+      .status(400)
+      .send("The warehouse ID you have provided is invalid. Please try again.");
+  }
+});
+
+module.exports = router;
