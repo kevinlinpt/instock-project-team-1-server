cb53e7b11d39d71e8267200cfd96fc117adf5477
kevinlinpt <kevinlinpt+1@hotmail.com>
2022-10-18 23:23:49 -0400

    changed DELETE inventory API to use fs read and write method

--- routes/DeleteInventoryItem/DeleteInventoryItem.js
+++ routes/DeleteInventoryItem/DeleteInventoryItem.js
@@ -1,16 +1,33 @@
-const express = require("express")
-const router = express.Router()
-const inventoryItems = require('../../data/inventories.json')
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
 
-router.delete('/',function(req,res){
-  if(req.body.id){
-    const inventoryItem = inventoryItems.find(
-      (item) => item.id === req.body.id
+let allInventoryItems;
+
+fs.readFile("./data/inventories.json", (err, data) => {
+  allInventoryItems = JSON.parse(data);
+});
+
+router.delete("/", function (req, res) {
+  if (req.body.id) {
+    const inventoryItems = allInventoryItems.filter(
+      (item) => item.id !== req.body.id
+    );
+    fs.writeFile(
+      "./data/inventories.json",
+      JSON.stringify(inventoryItems, null, 2),
+      (err) => {
+        res.send(err);
+      }
+    );
+    res.status(201).send(inventoryItems);
+  } else {
+    return res
+      .status(400)
+      .send(
+        "The inventory ID you have provided is invalid. Please try again.."
       );
-      res.send(inventoryItem)
-    } else{
-      return res.status(400).send('The inventory ID you have provided is invalid. Please try again.')
-    }
-})
+  }
+});
 
-module.exports = router
+module.exports = router;
