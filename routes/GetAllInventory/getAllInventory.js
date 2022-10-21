dd94187a46da8c7fd0fc5b993da9eb8fee5dbe81
falgunprajapati <falgunprajapati8@gmail.com>
2022-10-20 22:56:52 -0400

    added fs module

--- routes/GetAllInventory/getAllInventory.js
+++ routes/GetAllInventory/getAllInventory.js
@@ -1,8 +1,18 @@
 const express = require("express")
 const router = express.Router()
-const inventory = require("../../data/inventories.json")
+const fs = require("fs")
+
+let inventory
+
+fs.readFile("./data/inventories.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log(err)
+    return
+  }
+  inventory = JSON.parse(data)
+})
 
 router.get("/", function (req, res) {
-  res.send(inventory)
+  res.status(201).send(inventory)
 })
 module.exports = router
