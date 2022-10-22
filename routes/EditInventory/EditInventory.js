796a9a1384a92fd83bbb2c2b8d6c11508c773384
Caitlin Klosterman <97420891+croseklo@users.noreply.github.com>
2022-10-22 00:46:30 -0700

    router.post

--- routes/EditInventory/EditInventory.js
+++ routes/EditInventory/EditInventory.js
@@ -8,3 +8,32 @@ let allInventory;
 fs.readFile("./data/inventories.json", (err, data) => {
   allInventory = JSON.parse(data);
 });
+
+//express router ---> validation for body data
+router.post("/", function (req, res) {
+  const isValidBody = checkBody(req);
+
+  if (isValidBody) {
+    const restInventory = allInventory.filter(
+      (inventory) => inventory.id !== req.body.id
+    );
+    restInventory.push(req.body);
+
+    fs.writeFile(
+      "./data/inventories.json",
+      JSON.stringify(restInventory, null, 2),
+      (err) => {
+        res.send(err);
+      }
+    );
+    res.status(201).send(restInventory);
+  } else {
+    res
+      .status(400)
+      .send(
+        "Please provide all the values for inventory in the body of your request"
+      );
+  }
+});
+
+module.exports = router;
