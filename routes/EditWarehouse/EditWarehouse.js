9b367a788cb896a42dbf0f7a65e84e59ef603ea1
falgunprajapati <falgunprajapati8@gmail.com>
2022-10-23 01:53:47 -0400

    bug fixes

--- routes/EditWarehouse/EditWarehouse.js
+++ routes/EditWarehouse/EditWarehouse.js
@@ -1,42 +1,28 @@
-const express = require("express");
-const router = express.Router();
-const fs = require("fs");
-const checkBody = require('../helper')
+const express = require("express")
+const router = express.Router()
+const fs = require("fs")
+const checkBody = require("../helper")
 
-let allWarehouses;
+let allWarehouses
 
 fs.readFile("./data/warehouses.json", (err, data) => {
-  allWarehouses = JSON.parse(data);
-});
-
+  allWarehouses = JSON.parse(data)
+})
 
 router.post("/", function (req, res) {
-    // this function will check all the values are present on the body 
-  const isValidBody = checkBody(req)
+  const restWarehouses = allWarehouses.filter(
+    (warehouses) => warehouses.id !== req.body.id
+  )
 
-  if (isValidBody) {
+  restWarehouses.push(req.body)
 
-    const restWarehouses = allWarehouses.filter(
-      (warehouses) => warehouses.id !== req.body.id
-    );
-
-    restWarehouses.push(req.body);
-    
-
-    fs.writeFile(
-      "./data/warehouses.json",
-      JSON.stringify(restWarehouses, null, 2),
-      (err) => {
-        res.send(err);
-      }
-    );
-    res.status(201).send(restWarehouses);
-  } else {
-    res
-      .status(400)
-      .send(
-        "Please provide all the values for warehouse in the body of your request"
-      );
-  }
-});
-module.exports = router;
+  fs.writeFile(
+    "./data/warehouses.json",
+    JSON.stringify(restWarehouses, null, 2),
+    (err) => {
+      res.send(err)
+    }
+  )
+  res.status(201).send(restWarehouses)
+})
+module.exports = router
--- routes/NewWarehouse/createNewWarehouse.js
+++ routes/NewWarehouse/createNewWarehouse.js
@@ -1,45 +1,45 @@
-const express = require("express");
-const router = express.Router();
-const fs = require("fs");
-const validator = require("node-email-validation");
-const checkValidBody = require("../helper");
-let savedWarehouses;
+const express = require("express")
+const router = express.Router()
+const fs = require("fs")
+const validator = require("node-email-validation")
+const checkValidBody = require("../helper")
+let savedWarehouses
 fs.readFile("./data/warehouses.json", "utf-8", (err, data) => {
   if (err) {
-    console.log(err);
+    console.log(err)
   }
   try {
-    savedWarehouses = JSON.parse(data);
+    savedWarehouses = JSON.parse(data)
   } catch (error) {
-    console.log(error, "try catch");
+    console.log(error, "try catch")
   }
-});
+})
 
 router.post("/", function (req, res) {
-  const isValidBody = checkValidBody(req);
-  const validPhone = req.body.contact.phone.split("").length === 10;
-  const validEmail = validator.is_email_valid(req.body.contact.email);
+  const isValidBody = checkValidBody(req)
+  const validPhone = req.body.contact.phone.split("").length === 10
+  const validEmail = validator.is_email_valid(req.body.contact.email)
 
   if (isValidBody && validPhone && validEmail) {
-    savedWarehouses.push(req.body);
+    savedWarehouses.push(req.body)
 
     fs.writeFile(
       "./data/warehouses.json",
       JSON.stringify(savedWarehouses, null, 2),
       (err) => {
         if (err) {
-          console.log(err);
+          console.log(err)
         }
       }
-    );
-    return res.status(201).send(savedWarehouses);
+    )
+    return res.status(201).send(savedWarehouses)
   } else if (!validator.is_email_valid(req.body.contact.email)) {
-    return res.status(400).send(`Please provide the valid email addresss`);
+    return res.status(400).send(`Please provide the valid email addresss`)
   } else if (!validPhone) {
-    return res.status(400).send("please provide a valid phone number");
+    return res.status(400).send("please provide a valid phone number")
   } else {
-    return res.status(400).send("Please provide the appropriate details");
+    return res.status(400).send("Please provide the appropriate details")
   }
-});
+})
 
-module.exports = router;
+module.exports = router
